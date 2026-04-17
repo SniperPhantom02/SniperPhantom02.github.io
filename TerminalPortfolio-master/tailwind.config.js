69bb0405061e5c585e36e1d5929dfd3f4e746581
Ashutosh <sharmaashutosh2610@gmail.com>
2026-04-17 20:46:31 +0000

    Add files via upload

---
+++ TerminalPortfolio-master/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: 'all',
+  preserveHtmlElements: false,
+  content: ["./docs/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
