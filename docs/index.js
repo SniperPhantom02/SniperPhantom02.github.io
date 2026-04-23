6769e86dc02a84d77b1ff6b391564490b43b4dad
Ashutosh <sharmaashutosh2610@gmail.com>
2026-04-23 19:03:28 +0000

    Add GSAP scripts for text reveal effect

--- docs/index.js
+++ docs/index.js
@@ -1,7 +1,8 @@
 /* =========================
    TEXT REVEAL (NAME)
 ========================= */
-
+<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.2/gsap.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.2/ScrollTrigger.min.js"></script>
 const nameEl = document.querySelector(".name");
 
 if (nameEl) {
